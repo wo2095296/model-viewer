e2503d2793fe9a8d14f9e4af6bf1e2a985d292e7
chrismgeorge <chrismgeorge@users.noreply.github.com>
2020-10-29 21:44:08 +0000

    deploy: 8e724cdf20168e7b232372c8d6a26bbcf7764619

---
+++ lib/docs-and-examples/utils.d.ts
@@ -0,0 +1,3 @@
+export declare function switchPages(oldLocation: string, newLocation: string): void;
+export declare function init(docsOrExample: string): void;
+export declare function toggleSidebar(): void;
